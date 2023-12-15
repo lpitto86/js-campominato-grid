c231d2e27f7ff8302b066d330a91dabe528640e3
lpitto86 <luca.p481@gmail.com>
2023-12-15 17:08:02 +0100

    Finalizzazione

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,18 @@ function generateGrid() {
         const cell = document.createElement('div');
         cell.className = 'grid-cell';
         cell.textContent = i;
+
+        // Aggiungi l'evento di click a ogni cella
+        cell.addEventListener('click', () => handleCellClick(cell, i));
+
         gridContainer.appendChild(cell);
     }
+}
+
+function handleCellClick(cell, cellNumber) {
+    // Cambia il colore della cella cliccata
+    cell.style.backgroundColor = 'lightblue';
+
+    // Visualizza il numero della cella nella console
+    console.log(`Cella cliccata: ${cellNumber}`);
 }
